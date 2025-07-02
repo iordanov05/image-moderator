d7bc9b7d3cc7f571f4d0b17d0e57e2a4c811efac
Иорданов Павел Алексеевич <paiordanov@edu.hse.ru>
2025-07-03 01:41:34 +0300

    test: ensure dummy DEEPAI_API_KEY for settings in tests

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        populate_by_name=True,  
+        populate_by_name=True,
     )
 
 
